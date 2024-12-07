ea17111f7acf21ef121e199ec1dfcf0b3e472c73
Nick Ilin <ninikitos90@gmail.com>
2024-12-07 01:10:46 -0500

    Setting up starting template

---
+++ ar_business_cards/settings/base.py
@@ -0,0 +1,156 @@
+from pathlib import Path
+import dj_database_url
+from decouple import Csv, config
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# ==============================================================================
+# CORE SETTINGS
+# ==============================================================================
+
+SECRET_KEY = config('SECRET_KEY', default='django-insecure$ar_business_cards.settings.local')
+
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'ar_business_cards.apps.accounts',
+]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ROOT_URLCONF = 'ar_business_cards.urls'
+
+INTERNAL_IPS = ['127.0.0.1']
+
+WSGI_APPLICATION = 'ar_business_cards.wsgi.application'
+
+
+# ==============================================================================
+# MIDDLEWARE SETTINGS
+# ==============================================================================
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+# ==============================================================================
+# TEMPLATES SETTINGS
+# ==============================================================================
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+# ==============================================================================
+# DATABASES SETTINGS
+# ==============================================================================
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+    )
+}
+
+
+# ==============================================================================
+# AUTHENTICATION AND AUTHORIZATION SETTINGS
+# ==============================================================================
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# ==============================================================================
+# I18N AND L10N SETTINGS
+# ==============================================================================
+
+LANGUAGE_CODE = config('LANGUAGE_CODE', default='en-us')
+
+TIME_ZONE = config('TIME_ZONE', default='UTC')
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# ==============================================================================
+# STATIC FILES SETTINGS
+# ==============================================================================
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATIC_ROOT = BASE_DIR.parent.parent / 'staticfiles'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+
+# ==============================================================================
+# MEDIA FILES SETTINGS
+# ==============================================================================
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR.parent.parent / 'media'
+
+
+
+# ==============================================================================
+# THIRD-PARTY SETTINGS
+# ==============================================================================
+
+
+# ==============================================================================
+# FIRST-PARTY SETTINGS
+# ==============================================================================
+
+# SIMPLE_ENVIRONMENT = config('SIMPLE_ENVIRONMENT', default='local')
